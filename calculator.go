b0ca7fb14a9c824d7400a0d7a4f35d83206e2af9
Jakub Jarosz <jakub@jarosz.dev>
2021-02-03 08:25:55 +0200

    Add tests for Division function

--- calculator.go
+++ calculator.go
@@ -1,6 +1,8 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import "errors"
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -15,4 +17,13 @@ func Subtract(a, b float64) float64 {
 // Multiply takes two numbers and returns the product.
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+// Divide takes two numbers and return the result of division.
+// It returns error if the second number is 0 (division by zero).
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero")
+	}
+	return a / b, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -2,8 +2,23 @@ package calculator_test
 
 import (
 	"calculator"
+	"math/rand"
 	"testing"
+	"time"
 )
+
+// GenerateNums it is a helper function
+// that produces two float numbers.
+func GenerateNums() (float64, float64) {
+	// Make generator to produce different
+	// numbers each time it runs (make it nondeterministic).
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	a := r.Float64() * float64(r.Intn(10000))
+	b := r.Float64() * float64(r.Intn(10000))
+	return a, b
+}
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
@@ -23,6 +38,23 @@ func TestAdd(t *testing.T) {
 		got := calculator.Add(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("%s, want %f, got %f", tc.name, tc.want, got)
+		}
+	}
+}
+
+func TestAddRandom(t *testing.T) {
+	t.Parallel()
+
+	name := "Add randomly generated numbers"
+
+	for i := 0; i < 100; i++ {
+		a, b := GenerateNums()
+		want := a + b
+
+		got := calculator.Add(a, b)
+
+		if got != want {
+			t.Errorf("%s, Add(%f, %f) = %f, want: %f", name, a, b, got, want)
 		}
 	}
 }
@@ -70,4 +102,37 @@ func TestMultiply(t *testing.T) {
 			t.Errorf("%s, want %f, got %f", tc.name, tc.want, got)
 		}
 	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	tt := []struct {
+		name        string
+		a           float64
+		b           float64
+		want        float64
+		expectedErr bool
+	}{
+		{name: "Divide two positive numbers", a: 6, b: 2, want: 3, expectedErr: false},
+		{name: "Divide two negative numbers", a: -10, b: -5, want: 2, expectedErr: false},
+		{name: "Divide two negative fraction numbers", a: -10.5, b: -5, want: 2.1, expectedErr: false},
+		{name: "Divide one positive, one negative number", a: -10, b: 5, want: -2, expectedErr: false},
+		{name: "Divide 0 by a number", a: 0, b: 5, want: 0, expectedErr: false},
+		{name: "Divide positive number by 0", a: 4, b: 0, want: 0, expectedErr: true},
+		{name: "Divide negative number by 0", a: -2, b: 0, want: 0, expectedErr: true},
+	}
+
+	for _, tc := range tt {
+		got, err := calculator.Divide(tc.a, tc.b)
+
+		if err != nil && !tc.expectedErr {
+			t.Errorf("%s, Divide(%f, %f) should return error", tc.name, tc.a, tc.b)
+		}
+
+		if got != tc.want && !tc.expectedErr {
+			t.Errorf("%s, Divide(%f, %f) = %f, want %f", tc.name, tc.a, tc.b, got, tc.want)
+		}
+	}
+
 }
