4aeac070b6a8ecad024843691f2009fb33bb08d8
Jakub Jarosz <jakub@jarosz.dev>
2021-02-03 20:48:31 +0200

    Functions accept multiple parameters

--- calculator.go
+++ calculator.go
@@ -6,42 +6,86 @@ import (
 	"math"
 )
 
-// Add takes 0 or more numbers and returns the result of adding them together.
-func Add(a ...float64) float64 {
-	var sum float64
+var (
+	// errNotEnoughNumbers is returned by functions that expects
+	// minimum of two arguments.
+	errNotEnoughNumbers = errors.New("minimum two numbers are required")
+
+	errDivisionByZero     = errors.New("division by zero")
+	errSqrtNumberNegative = errors.New("sqrt from negative number")
+)
+
+// Add takes 2 or more numbers and returns the result of adding them together.
+// If the number of arguments is less than two the function will return an error.
+func Add(a ...float64) (float64, error) {
+	if len(a) < 2 {
+		return 0, errNotEnoughNumbers
+	}
+	var result float64
 
 	for _, i := range a {
-		sum = sum + i
+		result = result + i
 	}
 
-	return sum
+	return result, nil
 }
 
-// Subtract takes two numbers and returns the result of subtracting the second
-// from the first.
-func Subtract(a, b float64) float64 {
-	return a - b
-}
-
-// Multiply takes two numbers and returns the product.
-func Multiply(a, b float64) float64 {
-	return a * b
-}
-
-// Divide takes two numbers and return the result of division.
-// It returns error if the second number is 0 (division by zero).
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, errors.New("error: division by zero")
+// Subtract takes two or more numbers and returns the result
+// of subtracting them or error, if the number of arguments
+// is less than two.
+func Subtract(a ...float64) (float64, error) {
+	if len(a) < 2 {
+		return 0, errNotEnoughNumbers
 	}
-	return a / b, nil
+	result := a[0]
+
+	for i := 1; i < len(a); i++ {
+		result = result - a[i]
+	}
+
+	return result, nil
 }
 
-// Sqrt takes a number and returns square root if the number
-// is >= 0. If number is < 0 the function returns an error.
+// Multiply takes min two numbers and returns the product
+// or error, if the number of arguments is less than two.
+func Multiply(a ...float64) (float64, error) {
+	if len(a) < 2 {
+		return 0, errNotEnoughNumbers
+	}
+	result := a[0]
+
+	for i := 1; i < len(a); i++ {
+		result = result * a[i]
+	}
+
+	return result, nil
+}
+
+// Divide takes n numbers and return the result of division.
+// It returns error if the n+1 argument is equal 0 (division by zero)
+// or if the numer of arguments is less than 2.
+func Divide(a ...float64) (float64, error) {
+	if len(a) < 2 {
+		return 0, errNotEnoughNumbers
+	}
+
+	result := a[0]
+
+	for i := 1; i < len(a); i++ {
+		if a[i] == 0 {
+			return 0, errDivisionByZero
+		}
+		result = result / a[i]
+	}
+
+	return result, nil
+}
+
+// Sqrt takes a number and returns square root if the number.
+// If the number is < 0 the function returns an error.
 func Sqrt(a float64) (float64, error) {
 	if a < 0 {
-		return 0, errors.New("error: sqrt from negative number")
+		return 0, errSqrtNumberNegative
 	}
 	return math.Sqrt(a), nil
 }
