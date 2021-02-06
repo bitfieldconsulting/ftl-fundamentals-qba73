f26c494b3ad69152b6ea52e07b9d3949cb552742
Jakub Jarosz <jakub@jarosz.dev>
2021-02-06 10:12:48 +0200

    Apply code review feedback and refactoring

--- calculator_test.go
+++ calculator_test.go
@@ -3,50 +3,43 @@ package calculator_test
 import (
 	"calculator"
 	"math/rand"
+	"strings"
 	"testing"
 	"time"
 )
 
-// GenerateNums it is a helper function
-// that produces a slice of two float numbers.
+// testCese represents input data and expected output
+// from the function under test.
+type testCase struct {
+	name        string
+	a           float64
+	b           float64
+	c           []float64
+	want        float64
+	expectedErr bool
+}
+
+// GenerateNums it is a helper function that produces a slice
+// of minimum two and maximum 100 float numbers.
 func GenerateNums() []float64 {
 	// Make generator to produce different
 	// numbers each time it runs (make it nondeterministic).
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
 
-	a := r.Float64() * float64(r.Intn(10000))
-	b := r.Float64() * float64(r.Intn(10000))
-	return []float64{a, b}
-}
+	// Max & Min represent maximum and minimum numbers to generate.
+	min, max := 2, 100
+	// Make sure we have at least 0 + min numbers!
+	n := r.Intn(max-min) + min
+	nums := make([]float64, n)
 
-func TestAdd(t *testing.T) {
-	t.Parallel()
-
-	tt := []struct {
-		name        string
-		nums        []float64
-		want        float64
-		expectedErr bool
-	}{
-		{name: "Add two positive numbers", nums: []float64{1, 2}, want: 3, expectedErr: false},
-		{name: "Add two negative numbers", nums: []float64{-4.5, -5.5}, want: -10, expectedErr: false},
-		{name: "Add multiple numbers", nums: []float64{-4.5, -5.5, 10, 2.5}, want: 2.5, expectedErr: false},
-		{name: "Add no numbers", nums: []float64{}, want: 0, expectedErr: true},
-		{name: "Add one number", nums: []float64{10}, want: 10, expectedErr: true},
+	// Populate slice with float numbers.
+	for i := 0; i < n; i++ {
+		fl := r.Float64() * float64(r.Intn(10000))
+		nums[i] = fl
 	}
 
-	for _, tc := range tt {
-		got, err := calculator.Add(tc.nums...)
-
-		if err != nil && !tc.expectedErr {
-			t.Errorf("%s Add(%v) should return an error", tc.name, tc.nums)
-		}
-
-		if tc.want != got && !tc.expectedErr {
-			t.Errorf("%s Add(%v) = %f, want %f", tc.name, tc.nums, got, tc.want)
-		}
-	}
+	return nums
 }
 
 func TestAddRandom(t *testing.T) {
@@ -56,142 +49,134 @@ func TestAddRandom(t *testing.T) {
 
 	for i := 0; i < 100; i++ {
 		a := GenerateNums()
-		want := a[0] + a[1]
+		var want float64
+		var args []float64
 
-		// Do not check err here as in this test
-		// we always pass two params to the function.
-		got, _ := calculator.Add(a...)
+		for _, i := range a {
+			want += i
+		}
+
+		// If we generated more than required minimum params
+		// then we will pass them to Add() func as optional args.
+		if len(a) > 2 {
+			args = a[2:]
+		}
+
+		got := calculator.Add(a[0], a[1], args...)
 
 		if got != want {
-			t.Errorf("%s, Add(%v) = %f, want: %f", name, a, got, want)
+			t.Errorf("%s; Add() = %f; want: %f", name, got, want)
 		}
 	}
 }
 
-func TestSubtract(t *testing.T) {
+func TestCalculator(t *testing.T) {
 	t.Parallel()
 
-	tt := []struct {
-		name        string
-		nums        []float64
-		want        float64
-		expectedErr bool
-	}{
-		{name: "Substract two positive numbers", nums: []float64{4, 2}, want: 2, expectedErr: false},
-		{name: "Substract two negative numbers", nums: []float64{-4.5, -5.5}, want: 1, expectedErr: false},
-		{name: "Substract from zero", nums: []float64{0, 5.45}, want: -5.45, expectedErr: false},
-		{name: "Substract multiple numbers", nums: []float64{20, 5.5, 4, 3, 1.25}, want: 6.25, expectedErr: false},
-		{name: "Substract incorrect number of params", nums: []float64{4}, want: 0, expectedErr: true},
-		{name: "Substract incorrect number of params", nums: []float64{}, want: 0, expectedErr: true},
+	tt := []testCase{
+		{name: "Add two positive numbers", a: 1, b: 2, want: 3},
+		{name: "Add two negative numbers", a: -4.5, b: -5.5, want: -10},
+		{name: "Add three negative numbers", a: -4.5, b: -5.5, c: []float64{-4.5}, want: -14.5},
+		{name: "Add multiple numbers", a: 2, b: 3.4, c: []float64{-4.5, -5.5, 10, 2.5}, want: 7.9},
+
+		{name: "Multiply three positive", a: 3, b: 2, c: []float64{2}, want: 12},
+		{name: "Multiply two positive one negative", a: 3, b: 2, c: []float64{-2}, want: -12},
+		{name: "Multiply one positive two negative", a: -3, b: 2, c: []float64{-2.5}, want: 15},
+		{name: "Multiply multiple numbers", a: 3, b: 2, c: []float64{2, -1}, want: -12},
+		{name: "Multiply two negative numbers", a: 2.5, b: 1.5, c: []float64{-4, -5}, want: 75},
+		{name: "Multiply by zero", a: 3, b: 4, c: []float64{0, -4}, want: 0},
+
+		{name: "Subtract two positive numbers", a: 4, b: 2, want: 2},
+		{name: "Subtract two negative numbers", a: -4.5, b: -5.5, want: 1},
+		{name: "Subtract from zero", a: 0, b: 5.45, want: -5.45},
+		{name: "Subtract multiple numbers", a: 10, b: 3, c: []float64{2, 1, 4, 3}, want: -3},
+		{name: "Subtract multiple numbers with negative", a: 10.5, b: 3, c: []float64{20, 5.5, 4, -3, 1.25}, want: -20.25},
 	}
 
 	for _, tc := range tt {
-		got, err := calculator.Subtract(tc.nums...)
+		t.Run(tc.name, func(t *testing.T) {
 
-		if err != nil && !tc.expectedErr {
-			t.Errorf("%s, Substract(%v) should return an error", tc.name, tc.nums)
-		}
+			// we need to decide which calculator func to run. I took approach
+			// of not adding a new field in the test struct (to differentiate
+			// function test data) but to keep test name consistent -
+			// always start with the calc function name. This way I can
+			// parse name, extract the function name and use it in the switch
+			// statement.
+			fname := strings.Split(tc.name, " ")[0]
+			var got float64
 
-		if tc.want != got && !tc.expectedErr {
-			t.Errorf("%s, Substract(%v) = %f, want %f", tc.name, tc.nums, got, tc.want)
-		}
-	}
-}
+			switch fname {
+			case "Add":
+				got = calculator.Add(tc.a, tc.b, tc.c...)
+			case "Multiply":
+				got = calculator.Multiply(tc.a, tc.b, tc.c...)
+			case "Subtract":
+				got = calculator.Subtract(tc.a, tc.b, tc.c...)
+			}
 
-func TestMultiply(t *testing.T) {
-	t.Parallel()
-
-	tt := []struct {
-		name        string
-		nums        []float64
-		want        float64
-		expectedErr bool
-	}{
-		{name: "Multiply three positive", nums: []float64{3, 2, 2}, want: 12, expectedErr: false},
-		{name: "Multiply two positive one negative", nums: []float64{3, 2, -2}, want: -12, expectedErr: false},
-		{name: "Multiply one positive two negative", nums: []float64{-3, 2, -2}, want: 12, expectedErr: false},
-		{name: "Multiply two positive numbers", nums: []float64{3, 2}, want: 6, expectedErr: false},
-		{name: "Multiply two negative numbers", nums: []float64{-4, -5}, want: 20, expectedErr: false},
-		{name: "Multiply one negative one positive", nums: []float64{-4, 5}, want: -20, expectedErr: false},
-		{name: "Multiply by zero", nums: []float64{-4, 0}, want: 0, expectedErr: false},
-		{name: "Multiply zero args", nums: []float64{}, want: 0, expectedErr: true},
-		{name: "Multiply one arg", nums: []float64{-4}, want: 0, expectedErr: true},
-	}
-
-	for _, tc := range tt {
-		got, err := calculator.Multiply(tc.nums...)
-
-		if err != nil && !tc.expectedErr {
-			t.Errorf("%s, Multiply(%v) should return an error", tc.name, tc.nums)
-		}
-
-		if tc.want != got {
-			t.Errorf("%s, want %f, got %f", tc.name, tc.want, got)
-		}
+			if got != tc.want {
+				t.Errorf("%s; %s(%f, %f, %v) = %f; want %f", tc.name, fname, tc.a, tc.b, tc.c, got, tc.want)
+			}
+		})
 	}
 }
 
 func TestDivide(t *testing.T) {
 	t.Parallel()
 
-	tt := []struct {
-		name        string
-		nums        []float64
-		want        float64
-		expectedErr bool
-	}{
-		{name: "Divide two positive numbers", nums: []float64{6, 2}, want: 3, expectedErr: false},
-		{name: "Divide two negative numbers", nums: []float64{-10, -5}, want: 2, expectedErr: false},
-		{name: "Divide two negative fraction numbers", nums: []float64{-10.5, -5}, want: 2.1, expectedErr: false},
-		{name: "Divide one positive, one negative number", nums: []float64{-10, 5}, want: -2, expectedErr: false},
-		{name: "Divide 0 by a number", nums: []float64{0, 5}, want: 0, expectedErr: false},
-		{name: "Divide only one argument", nums: []float64{4}, want: 0, expectedErr: true},
-		{name: "Divide zero agruments", nums: []float64{}, want: 0, expectedErr: true},
-		{name: "Divide positive number by 0", nums: []float64{4, 0}, want: 0, expectedErr: true},
-		{name: "Divide positive number by 0", nums: []float64{4, 3, 0}, want: 0, expectedErr: true},
-		{name: "Divide positive number by 0", nums: []float64{4, 3, 5, 0}, want: 0, expectedErr: true},
-		{name: "Divide positive number by 0", nums: []float64{4, 3, 0, 1}, want: 0, expectedErr: true},
-		{name: "Divide negative number by 0", nums: []float64{-2, 0}, want: 0, expectedErr: true},
+	tt := []testCase{
+		{name: "Divide two positive numbers", a: 6, b: 2, c: []float64{}, want: 3, expectedErr: false},
+		{name: "Divide two negative numbers", a: -10, b: -5, want: 2, expectedErr: false},
+		{name: "Divide two negative fraction numbers", a: -10.5, b: -5, want: 2.1, expectedErr: false},
+		{name: "Divide one positive, one negative number", a: -10, b: 2.5, want: -4, expectedErr: false},
+		{name: "Divide 0 by a number", a: 0, b: 5, want: 0, expectedErr: false},
+		{name: "Divide by zero", a: 2.5, b: 0.5, c: []float64{2, 1.25, 1}, want: 2, expectedErr: false},
+
+		// Test cases with expected errors.
+		{name: "Divide by zero", a: 2, b: 0, want: 0, expectedErr: true},
+		{name: "Divide by zero", a: 2, b: 1, c: []float64{2, 0, 1}, want: 0, expectedErr: true},
 	}
 
 	for _, tc := range tt {
-		got, err := calculator.Divide(tc.nums...)
+		t.Run(tc.name, func(t *testing.T) {
 
-		if err != nil && !tc.expectedErr {
-			t.Errorf("%s, Divide(%v) should return error", tc.name, tc.nums)
-		}
+			got, err := calculator.Divide(tc.a, tc.b, tc.c...)
 
-		if got != tc.want && !tc.expectedErr {
-			t.Errorf("%s, Divide(%v) = %f, want %f", tc.name, tc.nums, got, tc.want)
-		}
+			if (err != nil) != tc.expectedErr {
+				t.Fatalf("%s; Divide(%f, %f, %v) should return error", tc.name, tc.a, tc.b, tc.c)
+			}
+
+			if got != tc.want && !tc.expectedErr {
+				t.Errorf("%s; Divide(%f, %f, %v) = %f; want %f", tc.name, tc.a, tc.b, tc.c, got, tc.want)
+			}
+		})
 	}
 }
 
 func TestSqrt(t *testing.T) {
 	t.Parallel()
 
-	tt := []struct {
-		name        string
-		a           float64
-		want        float64
-		expectedErr bool
-	}{
+	tt := []testCase{
 		{name: "Sqrt positive number", a: 9, want: 3, expectedErr: false},
 		{name: "Sqrt 0 number", a: 0, want: 0, expectedErr: false},
+
+		// Test cases with expected error.
 		{name: "Sqrt negative number", a: -4, want: 0, expectedErr: true},
 		{name: "Sqrt negative number", a: -16.5, want: 0, expectedErr: true},
 	}
 
 	for _, tc := range tt {
-		got, err := calculator.Sqrt(tc.a)
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := calculator.Sqrt(tc.a)
 
-		if err != nil && !tc.expectedErr {
-			t.Errorf("%s, Sqrt(%f) should return error", tc.name, tc.a)
-		}
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("%s; Sqrt(%f) should return error", tc.name, tc.a)
+			}
 
-		if got != tc.want && !tc.expectedErr {
-			t.Errorf("%s, Sqrt(%f) = %f, want %f", tc.name, tc.a, got, tc.want)
-		}
+			if got != tc.want && !tc.expectedErr {
+				t.Errorf("%s; Sqrt(%f) = %f; want %f", tc.name, tc.a, got, tc.want)
+			}
+		})
 	}
 }
 
@@ -206,30 +191,32 @@ func TestCompute(t *testing.T) {
 	}{
 		{name: "Compute sum", expression: "2 + 3", want: 5, expectedErr: false},
 		{name: "Compute sum, no float", expression: "2 + 4", want: 6, expectedErr: false},
-		{name: "Compute substract, no float", expression: "5 - 2", want: 3, expectedErr: false},
-		{name: "Compute multiply, no float", expression: "4 * -4", want: -16, expectedErr: false},
-		{name: "Compute multiply, no float", expression: "3 * -2", want: -6, expectedErr: false},
-		{name: "Compute multiply, no float", expression: "-3 * 2", want: -6, expectedErr: false},
-		{name: "Compute divide, no float", expression: "10 / 2", want: 5, expectedErr: false},
+		{name: "Compute substract no float", expression: "5 - 2", want: 3, expectedErr: false},
+		{name: "Compute multiply no float", expression: "4 * -4", want: -16, expectedErr: false},
+		{name: "Compute multiply no float", expression: "3 * -2", want: -6, expectedErr: false},
+		{name: "Compute multiply no float", expression: "-3 * 2", want: -6, expectedErr: false},
+		{name: "Compute divide no float", expression: "10 / 2", want: 5, expectedErr: false},
 		{name: "Compute additional spaces", expression: "2  + 4 ", want: 6, expectedErr: false},
 		{name: "Compute additional spaces", expression: " 2  +  4  ", want: 6, expectedErr: false},
 		{name: "Compute additional spaces", expression: "  2    +  -4  ", want: -2, expectedErr: false},
 
-		// Negative cases. They should return error.
+		// Test cases with expected error.
 		{name: "Compute incorrect operator", expression: " 10 & 2   ", want: 0, expectedErr: true},
 		{name: "Compute incorrect operator", expression: " 10  #  2", want: 0, expectedErr: true},
 		{name: "Compute incorrect operator", expression: "10 & 2", want: 0, expectedErr: true},
 	}
 
 	for _, tc := range tt {
-		got, err := calculator.Compute(tc.expression)
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := calculator.Compute(tc.expression)
 
-		if err != nil && !tc.expectedErr {
-			t.Errorf("%s, Compute(%s) = %v, expected error", tc.name, tc.expression, err)
-		}
+			if (err != nil) != tc.expectedErr {
+				t.Errorf("%s; Compute(%s) = %v; expected error", tc.name, tc.expression, err)
+			}
 
-		if got != tc.want && !tc.expectedErr {
-			t.Errorf("%s, Compute(%s) = %f, want %f", tc.name, tc.expression, got, tc.want)
-		}
+			if got != tc.want && !tc.expectedErr {
+				t.Errorf("%s; Compute(%s) = %f; want %f", tc.name, tc.expression, got, tc.want)
+			}
+		})
 	}
 }
